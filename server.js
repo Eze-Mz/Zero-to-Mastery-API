31a0bea24bc18e74ca6fed5490513055fbc2720f
Eze Muñoz <eze.munoz@mi.unc.edu.ar>
2023-03-23 20:49:22 -0300

    database connection fixed

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ const db = knex({
   debug: true
 });
 
-knex.raw('select 1+1 as result').then(function () {
-  console.log("succes!");
-});
-
 // use select with knex to de database
 // console.log(
 //   db
